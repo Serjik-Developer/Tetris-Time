5a649c20d92d861c0ee40a1ed1c7bb122668467c
Artem Potapov <strim20092509@gmail.com>
2025-04-08 15:25:01 +0300

    add db classes

---
+++ app/src/main/java/com/example/test_techonstrelka/datarepo/BaseHelper.java
@@ -0,0 +1,11 @@
+package com.example.test_techonstrelka.datarepo;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+public class BaseHelper {
+}
+
---
+++ app/src/main/java/com/example/test_techonstrelka/datarepo/repositories/MainRepository.java
@@ -0,0 +1,5 @@
+package com.example.test_techonstrelka.datarepo.repositories;
+
+public class MainRepository {
+
+}
